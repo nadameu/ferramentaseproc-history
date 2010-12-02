404942fc7c34fe476dbc4cd1c565be88f989b477
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2010-12-02 21:21:36 +0000

    Acrescentados modelos para digitação de documentos no V2
    git-svn-id: http://ferramentas-e-proc.googlecode.com/svn/eproc/branches/0.7@126 0c9fecb3-505a-5f67-1bd6-d8981faec6f2

--- chrome/content/eprocV2.js
+++ chrome/content/eprocV2.js
@@ -325,6 +325,70 @@ var Eproc = {
         }
     },
     // }}}
+    // {{{ digitar_documento()
+    digitar_documento: function()
+    {
+        if (null == document.getElementById('txt_fck___Frame')) return;
+        var infoWindow = unsafeWindow.opener;
+        if (infoWindow) {
+            var info = infoWindow.document.getElementById('tbInfoProcesso');
+            if (info) {
+                var processo;
+                for (var labels = info.getElementsByTagName('label'), i = 0, label; label = labels[i]; i++) {
+                    if (label.textContent == 'Processo:') {
+                        processo = label.nextSibling.textContent;
+                    }
+                }
+                var autor = info.rows[1].cells[2].innerHTML.replace(/<br[^>]*>/g, ' ');
+                var reu = info.rows[1].cells[3].innerHTML.replace(/<br[^>]*>/g, ' ');
+            }
+        }
+        function criaBotao (sTexto, sTitulo, sConteudo, iTipo, oElemento)
+        {
+            var oBotao = document.createElement('button');
+            oBotao.innerHTML = sTexto;
+            oBotao.addEventListener('click', function(evt)
+            {
+                var oTexto = unsafeWindow.FCKeditorAPI.GetInstance('txt_fck');
+                if (!oTexto.IsDirty() || confirm('Todo o texto já digitado será apagado.\nConfirma?')) {
+                    oTexto.SetHTML('<html lang="pt-BR" dir="ltr"><head><title>' + sTitulo.replace(/<[^>]+>/g, '') + '</title><style type="text/css">.header { font-family: Helvetica; font-size: 10pt; } .title { font-family: Times; font-size: 14pt; font-weight: bold; } .text { font-family: Times; font-size: 13pt; } .signature { font-family: Times; font-size: 12pt; font-weight: bold; font-style: italic; } .dados { font-family: Times; font-size: 13pt; font-weight: bold; }</style></head><body bgcolor="white"><div class="header" align="center"><img width="85" height="86" src="http://eproc.trf4.gov.br/eproc2trf4/imagens/brasao_pb.jpg"></div><div class="header" align="center">PODER JUDICIÁRIO</div><div class="header" align="center"><strong>JUSTIÇA FEDERAL</div><div class="header" align="center"></strong>' + GM_getValue('v1.secao') + '</div><div class="header" align="center">' + GM_getValue('v1.subsecao') + '</div><div class="header" align="center">' + GM_getValue('v1.vara') + '</div><p class="text" align="justify">&nbsp;</p>' + (info ? '<div class="dados" align="left">PROCESSO: ' + processo + '</div><div class="dados" align="left">AUTOR: ' + autor + '</div><div class="dados" align="left">RÉU: ' + reu + '</div><p class="text" align="justify">&nbsp;</p>' : '') + '<p class="title" align="center">' + sTitulo + '</p><p class="text" align="justify">&nbsp;</p><p class="text" align="justify">' + sConteudo + '</p><p class="text" align="justify">&nbsp;</p><p class="text" align="justify">&nbsp;</p><p class="text" align="justify">&nbsp;</p><p class="signature" align="center">documento assinado eletronicamente</p></body></html>');
+                    document.getElementById('selTipoArquivo').value = iTipo;
+                }
+            }, true);
+            document.body.insertBefore(oBotao, oElemento);
+        }
+        if (screen.availWidth >= 780 && screen.availHeight >= 630) {
+            var w = 780;
+            var h = Math.floor((screen.availHeight - 30) / 100) * 100 + 30;
+            document.getElementById('txt_fck___Frame').height = h - 330;
+            window.moveTo((screen.availWidth - w) / 2, (screen.availHeight - h) / 2);
+            window.resizeTo(w, h);
+        }
+        document.body.insertBefore(document.createTextNode(' '), document.body.firstChild);
+        criaBotao('Sentença', 'SENTENÇA', 'TextoDaSentença', '14', document.body.firstChild);
+        criaBotao('Despacho', 'DESPACHO', 'TextoDoDespacho', '15', document.body.firstChild);
+        criaBotao('Decisão', 'DECISÃO', 'TextoDaDecisão', '32', document.body.firstChild);
+        criaBotao('Certidão', 'CERTIDÃO', 'CERTIFICO que .', '16', document.body.firstChild);
+        criaBotao('Ato', 'ATO DE SECRETARIA', 'De ordem do MM. Juiz Federal, a Secretaria da Vara .', '18', document.body.firstChild);
+    },
+    // }}}
+    // {{{ digitar_documento_oncomplete()
+    digitar_documento_oncomplete: function(ed)
+    {
+        ed.Events.AttachEvent('OnAfterSetHTML', function(e) { e.ResetIsDirty(); });
+        ed.Config.FullPage = true;
+        ed.Config.ToolbarSets['eProcv2'] = [
+            ['Cut','Copy','Paste','PasteText','PasteWord'],
+            ['Undo','Redo'],
+            ['Bold','Italic','Underline'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+            ['OrderedList','UnorderedList'],
+            ['TextColor'],
+            ['Source']
+        ];
+        ed.ToolbarSet.Load('eProcv2');
+    },
+    // }}}
     // {{{ entrar()
     entrar: function()
     {
@@ -636,6 +700,9 @@ var Eproc = {
             this[this.parametros.acao_origem + '_destino']();
         } else if (location.pathname.match(/\/eproc(V2|v2_homologacao|v2_apresentacao)\/(index.php)?/)) {
             document.createElement('img').src = '/infra_css/imagens/fndtransp.gif';
+        }
+        if (location.pathname.match(/\/class\/fckeditor\/editor\/fckeditor\.html/)) {
+            unsafeWindow.parent.FCKeditor_OnComplete = this.digitar_documento_oncomplete;
         }
     },
     // }}}
