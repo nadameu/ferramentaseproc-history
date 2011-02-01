dfcd81db42c4d74780641a8f463983510cec97f8
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2011-02-01 17:11:38 +0000

    * Renumeração da versão para 0.7.11
    git-svn-id: http://ferramentas-e-proc.googlecode.com/svn/eproc/branches/0.7@149 0c9fecb3-505a-5f67-1bd6-d8981faec6f2

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,4 +1,4 @@
-pref('general.useragent.extra.eproc', 'eproc-0.7.10');
+pref('general.useragent.extra.eproc', 'eproc-0.7.11');
 pref('eproc.v1.enable', true);
 pref('eproc.v1.consulta_processo.enable', true);
 pref('eproc.v1.html_to_pdf.enable', true);
