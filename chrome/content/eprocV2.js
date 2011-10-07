6f7bd21752a374315e9c68be83c90e850683322c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2011-10-07 18:19:47 +0000

    * Correção de CSS para impressão
    git-svn-id: http://ferramentas-e-proc.googlecode.com/svn/eproc/trunk@291 0c9fecb3-505a-5f67-1bd6-d8981faec6f2

--- chrome/content/eprocV2.js
+++ chrome/content/eprocV2.js
@@ -950,6 +950,10 @@ var Eproc = {
                 Eproc.addCssRule(rule);
             });
         }
+        document.body.setAttribute('onload', document.body.getAttribute('onload').replace('infraProcessarResize();', ''));
+        if (unsafeWindow.infraResize instanceof Function) {
+            window.removeEventListener('resize', unsafeWindow.infraResize, false);
+        }
         resetStyles();
         this.pagina = location.pathname.split('/eprocV2/')[1];
         this.parametros = {};
